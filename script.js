23f873457da4d4028df48c569a1d6d8e8604cecc
Devin Beliveau <devinmarieb@gmail.com>
2016-10-24 20:15:17 -0600

    Add reset.css. Start styling

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ CreateIdea.prototype.displayIdea = function () {
   $('.idea-list').prepend(
    `<li ${this.id} class="new-idea">
    <h2 class="title-input">${this.title}</h2>
+   <button class="delete" type="button" name="delete" img src="images/delete.svg"></button>
    <p class="body-input">${this.body}</p>
    <p class="rating">quality: <span class="user-quality">swill</span></p>
-   <button class="delete" type="button" name="delete" img src="images/delete.svg"></button>
    <button class="up-vote" type="button" name="up-vote" img src="images/upvote.svg"></button>
    <button class="down-vote" type="button" name="down-vote" img src="images/downvote.svg"></button>
    </li>`
